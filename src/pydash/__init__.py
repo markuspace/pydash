ff78110557877f3c400300f985d9811d4873492f
robomark <robomark@pop-os.lan>
2026-04-12 21:04:47 -0600

    Convert pydash to installable Python package

---
+++ src/pydash/__init__.py
@@ -0,0 +1,3 @@
+from .transpiler import transpile, step1_replace_emdash, step2_attach_schemas, runtime
+
+__all__ = ["transpile", "step1_replace_emdash", "step2_attach_schemas", "runtime"]
---
+++ src/pydash/__main__.py
@@ -0,0 +1,3 @@
+from .transpiler import main
+
+main()
